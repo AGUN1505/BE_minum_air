b47f6a8a79eda22df6cd64d59a3a08d4d7bd417f
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-08-05 14:01:44 +0700

    refactor: perubahan pada table user

---
+++ app/Http/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+class Kernel
+{
+    protected $middlewareGroups = [
+        'api' => [
+            EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
